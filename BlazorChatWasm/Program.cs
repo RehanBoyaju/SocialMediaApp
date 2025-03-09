539345c044858405053022eb6c6153e97bf245ed
Rehan Boyaju <rehanboyaju523@gmail.com>
2025-03-09 08:17:51 +0545

    correct delete account bug

--- BlazorChatWasm/Program.cs
+++ BlazorChatWasm/Program.cs
@@ -11,7 +11,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.254.6:5000") });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5085") });
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
